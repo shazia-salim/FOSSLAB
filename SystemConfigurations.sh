f0306c1ce49f31e70e662c52e81551eccd228161
Shazia Salim <shaziasalim23@gmail.com>
2018-02-20 20:21:11 +0530

    First Commit

---
+++ SystemConfigurations.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Various System Configurations
+echo "User Name:$LOGNAME"
+echo "Current Shell:$SHELL"
+echo "Home Directory:$HOME"
+echo "Operating System:$OSTYPE"
+echo "Current Path setting:$PATH"
+echo "Current Working Directory:$PATH"
+echo "Currently logged users:"
+who --count
+
+
